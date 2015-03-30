b11b3c32e8d14b822e7d2e5a065d24c3e7481317
Guillaume Denormandie <denormandie.g@gmail.com>
2015-03-30 14:53:57 +0200

    Do not listen to changes on contenteditable

--- src/UndoManager.js
+++ src/UndoManager.js
@@ -88,8 +88,15 @@ function UndoManager(element) {
 	function _pushState(mutations) {
 		// If there is only one mutation with a type of 'characterData', the user is simply inserting one caracter (not a space)
 		// So do nothing
-		if(mutations && mutations.length === 1 && mutations[0].type == 'characterData') {
+		if(mutations && mutations.length === 1 && mutations[0].type === 'characterData') {
 			return;
+		}
+		
+		// Do not listen to changes on contenteditable
+		for(var i in mutations) {
+			if(mutations[i].type === 'attributes' && mutations[i].attributeName === 'contenteditable') {
+				return;
+			}
 		}
 		
 		// If we did some undoing, we need to destroy the redo history
